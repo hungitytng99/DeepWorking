eace17aabb512ab1710fa41cdefb102bf7ff3afd
Nam Vu <vungocnam0409@gmail.com>
2020-08-03 14:59:34 +0700

    Add(library weekView)

--- app/src/main/java/com/icthust/deepworking/ui/daily/DailyFragment.java
+++ app/src/main/java/com/icthust/deepworking/ui/daily/DailyFragment.java
@@ -9,17 +9,23 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.hust.library.WeekDayView;
 import com.icthust.deepworking.R;
 
 public class DailyFragment extends Fragment {
 
     private DailyViewModel dailyViewModel;
+    private WeekDayView weekDayView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dailyViewModel =
                 ViewModelProviders.of(this).get(DailyViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_daily, container, false);
+
         return root;
     }
+
+
 }
